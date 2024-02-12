e0bb27ee6c9319e26ee02b3cf087c3aa1dd3fb12
MD.Rafiul Haque <rafiulbinoy@gmail.com>
2024-02-12 23:46:27 +0600

    whole project is completed

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        primarySwatch: Colors.blue,
         useMaterial3: true,
       ),
       home: const HomePage(),
